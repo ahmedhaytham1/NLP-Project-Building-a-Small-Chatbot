00be3d0f17b27ef2ef0879c87ff90125ac216b98
Ahmed Haytham <96838163+ahmedhaytham1@users.noreply.github.com>
2025-12-15 00:48:21 +0200

    Create embeddings.py

---
+++ rag/embeddings.py
@@ -0,0 +1,16 @@
+from sentence_transformers import SentenceTransformer
+
+def load_embedding_model():
+    """
+    Load sentence transformer model
+    """
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+    return model
+
+
+def generate_embeddings(texts, model):
+    """
+    Generate embeddings for a list of texts
+    """
+    embeddings = model.encode(texts)
+    return embeddings
